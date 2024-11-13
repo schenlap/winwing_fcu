570d01acc61453d2ffe3aee3165821de488edc75
schenlap <memo5@gmx.at>
2024-11-13 17:52:45 +0100

    search for usblib
    in non standard directories to find
    usblib if homebrew installation.

--- winwing_fcu.py
+++ winwing_fcu.py
@@ -19,6 +19,7 @@ from threading import Thread, Event, Lock
 from time import sleep
 
 import usb.core
+import usb.backend.libusb1
 import usb.util
 
 import XPlaneUdp
@@ -517,6 +518,25 @@ def kb_wait_quit_event():
         os._exit(0)
 
 
+def find_usblib():
+    path = ['/opt/homebrew/lib/libusb-1.0.0.dylib',
+            '/usr/lib/x86_64-linux-gnu/libusb-1.0.so.0',
+            '/usr/lib/libusb-1.0.so.0']
+    pathlist = list(enumerate(path))
+    for p in range(len(pathlist)):
+        backend = usb.backend.libusb1.get_backend(find_library=lambda x: pathlist[p][1])
+        if backend:
+            print(f"using {pathlist[p][1]}")
+            return backend
+
+    print(f"*** No usblib found. Install it with:")
+    print(f"***   debian: apt install libusb1")
+    print(f"***   mac: brew install libusb")
+    print(f"***   If you get this warning and fcu is working, please open an issue at")
+    print(f"***   https://github.com/schenlap/winwing_fcu")
+    return None
+
+
 def main():
     global xp
     global fcu_in_endpoint, fcu_out_endpoint
@@ -524,7 +544,8 @@ def main():
 
     create_button_list_fcu()
 
-    device = usb.core.find(idVendor=0x4098, idProduct=0xbb10)
+    backend = find_usblib()
+    device = usb.core.find(idVendor=0x4098, idProduct=0xbb10, backend=backend)
     if device is None:
         raise RuntimeError('Winwing FCU-A320 not found')
     print('Found winwing FCU-A320')
